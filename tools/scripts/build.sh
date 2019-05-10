b1f8b7e32b8019bc5d1a7bd27a2e1daaee5677cb
Tane Piper <tane@backbase.com>
2019-05-10 18:54:12 +0200

    rename script

--- tools/scripts/build.sh
+++ tools/scripts/build.sh
@@ -7,6 +7,9 @@ set -o errexit -o noclobber -o nounset -o pipefail
 echo "Building Distribution"
 npm run build ngx-editorjs
 
+echo "Build Docs"
+compodoc -p libs/ngx-editorjs/tsconfig.lib.json -d libs/ngx-editorjs/docs
+
 echo "Copy Changelog"
 cp libs/ngx-editorjs/CHANGELOG.md dist/libs/ngx-editorjs/CHANGELOG.md
 
