17e4f2e0ab53035ab0bf2cf7afc5129920466b77
Tane Piper <tane@backbase.com>
2019-05-10 20:03:31 +0200

    Doc fixes

--- tools/scripts/build.sh
+++ tools/scripts/build.sh
@@ -7,11 +7,11 @@ set -o errexit -o noclobber -o nounset -o pipefail
 echo "Building Distribution"
 npm run build ngx-editorjs
 
-echo "Build Docs"
-compodoc -p libs/ngx-editorjs/tsconfig.lib.json -d libs/ngx-editorjs/docs
+# echo "Build Docs"
+# compodoc -p libs/ngx-editorjs/tsconfig.lib.json -d libs/ngx-editorjs/docs
 
 echo "Copy Changelog"
 cp libs/ngx-editorjs/CHANGELOG.md dist/libs/ngx-editorjs/CHANGELOG.md
 
-echo "Copy Docs"
-cp -r libs/ngx-editorjs/docs dist/libs/ngx-editorjs/docs
+# echo "Copy Docs"
+# cp -r libs/ngx-editorjs/docs dist/libs/ngx-editorjs/docs
