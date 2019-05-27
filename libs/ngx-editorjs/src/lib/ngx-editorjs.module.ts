266bdee4258402b33772da959d62e4a4a9e4b1fc
Tane Piper <tane@backbase.com>
2019-05-27 12:23:42 +0200

    Save point

--- libs/ngx-editorjs/src/lib/ngx-editorjs.module.ts
+++ libs/ngx-editorjs/src/lib/ngx-editorjs.module.ts
@@ -1,17 +1,16 @@
 import { CommonModule } from '@angular/common';
 import { ModuleWithProviders, NgModule, Optional, SkipSelf } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { MatFormFieldModule, MatInputModule } from '@angular/material';
 import EditorJS from '@editorjs/editorjs';
+import { NgxEditorJSToolbarComponent } from './components/editorjs-toolbar/editorjs-toolbar.component';
 import { createModuleConfig } from './config/module-config';
 import { NgxEditorJSMatFieldComponent } from './containers/editorjs-mat-field/editorjs-mat-field.component';
 import { NgxEditorJSComponent } from './containers/editorjs/editorjs.component';
 import { NgxEditorJSDirective } from './directives/ngx-editorjs.directive';
+import { NgxEditorJSMaterialModule } from './ngx-editorjs.material.module';
 import { NgxEditorJSService } from './services/editorjs.service';
 import { FOR_ROOT_OPTIONS_TOKEN, NgxEditorJSModuleConfig, NGX_EDITORJS_CONFIG } from './types/config';
 import { EDITORJS_INSTANCE, EDITORJS_MODULE_IMPORT } from './types/injector';
-import { NgxEditorJSToolbarComponent } from './components/editorjs-toolbar/editorjs-toolbar.component';
-import { NgxEditorJSMaterialModule } from './ngx-editorjs.material.module';
 
 /**
  * Factory function to return the EditorJS base class
