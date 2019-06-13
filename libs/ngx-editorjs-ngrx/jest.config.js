2943b5a3f51ab53e7841d02f7a45048f4e2e0e5a
Tane Piper <tane@backbase.com>
2019-06-13 19:13:42 +0200

    Move library in repo, prepare for next release

--- libs/ngx-editorjs-ngrx/jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  name: 'ngx-editorjs-ngrx',
-  preset: '../../jest.config.js',
-  coverageDirectory: '../../coverage/libs/ngx-editorjs-ngrx',
-  snapshotSerializers: [
-    'jest-preset-angular/AngularSnapshotSerializer.js',
-    'jest-preset-angular/HTMLCommentSerializer.js'
-  ]
-};
--- libs/ngx-editorjs-plugins/jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  name: 'ngx-editorjs-plugins',
-  preset: '../../jest.config.js',
-  coverageDirectory: '../../coverage/libs/ngx-editorjs-plugins',
-  snapshotSerializers: [
-    'jest-preset-angular/AngularSnapshotSerializer.js',
-    'jest-preset-angular/HTMLCommentSerializer.js'
-  ]
-};
